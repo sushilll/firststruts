b104935846c1891d2468145738031d3414ddf128
sushilll <sushil101094@gmail.com>
2017-03-30 21:24:30 +0530

    dao changed

--- src/firststruts/Dao.java
+++ src/firststruts/Dao.java
@@ -1,5 +1,7 @@
 package firststruts;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,11 +10,20 @@ public class Dao {
 	
 	public static Connection getConnection() {
 		Connection con = null;
-	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
-	    try {
-			con = DriverManager.getConnection(dbUrl);
+		 URI dbUri;
+		try {
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+		
+
+		    String username = dbUri.getUserInfo().split(":")[0];
+		    String password = dbUri.getUserInfo().split(":")[1];
+		    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+//	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+	    	con = DriverManager.getConnection(dbUrl, username, password);
+//			con = DriverManager.getConnection(dbUrl);
 			System.out.println("method called");
-		} catch (SQLException e) {
+		} catch (SQLException | URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
